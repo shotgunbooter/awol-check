cb958084620da7b2ddadb4a7a0beeb6c9f607b24
james warden <jwarden966@gmail.com>
2025-03-27 04:26:28 +1100

    manual user search

--- scripts.js
+++ scripts.js
@@ -41,3 +41,41 @@ document.getElementById("fetchData").addEventListener("click", async function() 
         console.error("Error fetching data:", error);
     }
 });
+
+document.getElementById("manual-check").addEventListener("click", async function() {
+    const tableBody = document.getElementById("manual-resutls");
+    const username = document.getElementById("manual-input").value
+
+    try {
+        const data = await document.getElementById("manual-input").innerText.toString()
+        tableBody.innerHTML = "";
+
+        const habboApiUrl = `https://www.habbo.com/api/public/users?name=${encodeURIComponent(username)}`;
+        const userResponse = await fetch(habboApiUrl);
+
+        if (userResponse.status == 404 || userResponse.status == 400) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${username}</td>
+                <td class="invalid-username">User does not exist on habbo!</td>
+            `;
+            tableBody.appendChild(row);
+        } else {
+            const userData = await userResponse.json();
+            
+            if (userData.lastAccessTime) {
+                const lastLogin = new Date(userData.lastAccessTime);
+                const daysSinceLogin = Math.floor((Date.now() - lastLogin) / (1000 * 60 * 60 * 24));
+                
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                <td>${username}</td>
+                <td class="${daysSinceLogin > 10 ? 'awol' : ''}">${daysSinceLogin}</td>
+                `;
+                tableBody.appendChild(row);
+            }
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+});
