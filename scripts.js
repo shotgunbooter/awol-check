2ee5415ddf40cf62b92cd0f3331d46533d848d5f
shotgunbooter <jwarden966@gmail.com>
2025-06-14 08:04:42 +1000

    BoS / NP Checker

--- scripts.js
+++ scripts.js
@@ -156,3 +156,249 @@ document.getElementById("validate-bgc").addEventListener("click", async function
     }
 });
 
+
+/* 
+################
+BOS / NP CHECKER
+################
+*/
+
+const boslist = {
+    "Interpol": {
+        "link": "https://inthabbo.net/ban-on-sight/",
+        "usernames": [
+            "LucyGxx29/carefulwtyousay/rosielee20161  ",
+            "victorgabr_90  ",
+            "jansen00-/jansen1234r/-Sharkboy-  ",
+            "tulikacullen/YallHeauxMad  ",
+            "ForgotMyDog  ",
+            "Ali21./Ali14.  ",
+            "hungryalifp  ",
+            "StarMendezzz  ",
+            "Renzooooooo  ",
+            ":.SecretElder.:  ",
+            "SpookySpider/COMAXIOM  ",
+            "samira..  ",
+            "Kearley  ",
+            "666leah  ",
+            "SarahSmithBAN  ",
+            "unlaced_  ",
+            "LittleDuckling  ",
+            "Lucy_is_ded/.bean.  ",
+            "Berat  ",
+            "queenclassy  ",
+            "AlphaReborn  ",
+            "pepe-maker  ",
+            "AlisonWiltshire/EvelynAlberry/JosalinaGolden  ",
+            "rensesme  ",
+            "Mannyy  ",
+            "Pokemoners/David:  ",
+            "StinkyObama/HotArjunHot  ",
+            "Bruxelles/DeLeTeD_UsEr  ",
+            "M_il_an/SIDD  ",
+            "=x=Nikki=x=  ",
+            "Johnny15Adam  ",
+            "nicegirls45  ",
+            "SlayerDune  "
+        ]
+    },
+    "United Nations": {
+        "link": "https://habboun.com/pages/ban-sight",
+        "usernames": [
+            "Crept",
+            "Harvoar",
+            "Apical",
+            "Vaporizer",
+            "Zorloft",
+            "Wxlshyy",
+            "-avocadorable",
+            "OmegaSubZero",
+            "-Charles",
+            "Lewis8261",
+            "AbsolutSlayer",
+            "-GoldBars.-",
+            "JKN27",
+            "Cronology",
+            "ItsMak",
+            "XxxTrayxxX",
+            "SGTDawson",
+            "PandaCrunch"
+        ]
+    },
+    "Secret Service": {
+        "link": "https://habboss.org/units/ea/np/",
+        "usernames": [
+            "Zealing",
+            "MyNameisB0aty",
+            "Stevo127",
+            "FBI-Operator",
+            "@Badges",
+            "TateLynn90",
+            "eric903",
+            "Torsten,",
+            "Jakeedog",
+            "Youngleen",
+            "Ailex (AIlex)",
+            "Yodasbathtub",
+            "Pete-P",
+            "Chandiee",
+            "Mostafaomar",
+            "Rubix",
+            "LLegs",
+            "Kevchi",
+            ".onomanopia",
+            "Will_VT",
+            "Mostafamoha.",
+            "liamkeats",
+            "Conditioners",
+            "Jordann::",
+            "Hopps_12345",
+            "nicolesky25",
+            "Sam_Lay123",
+            "Unykorn"
+        ]
+    },
+    "Habbo Defence Agency": {
+        "link": "https://www.hda-habbo.com/national-punishment.html",
+        "usernames": [
+            "HabibiOmar",
+            "jubxz",
+            "funkyshuck",
+            "SeargentElmo",
+            "Leiandros",
+            "KingTray18",
+            "GhostJoeliff/hopps_1234",
+            "RossGXC",
+            "Hiow",
+            "izzy3134",
+            "\,3p?",
+            "ANIMAX/ST.Arcumbs",
+            "Rockmanvincent",
+            "hollister312",
+            "Brian\,\,\,\,\,",
+            ".colourpencils/,gummyworms",
+            "Purigatory",
+            "Giloblastoma",
+            "Hiow...",
+            "HX7",
+            "JasonBourne_",
+            "Jiggle/Any Alts",
+            "KingJason3.",
+            "Lani956",
+            "louis3856",
+            "melwinthebig",
+            "michelle-xixu",
+            "Pabboi",
+            "PhDJayBryan",
+            "Skrosis",
+            "spyjthecrazy",
+            "TheLegendshere",
+            "TonyStar_",
+            "Toxicator",
+            "Txrbo/cvcifer",
+            "Kittyevpixie",
+            "Glenda.Smith",
+            "XLaCosaNostraX / jordy77863",
+            "sohelpmealex",
+            "StinkyObama / HotArjunHot / Any Alts",
+            ".OnomanopiaValore - alt Understudy",
+            ":-JamJar-:",
+            "tjw162",
+            "ChalIenge",
+            "Sebbi",
+            "Jiggle / Any Alts",
+            "funkyshuck",
+            "Klausmikaelson / Any Alts",
+            "Jordan:: / Any Alts",
+            "HDA-George / FlyDifferentNZ/georgeygeorge / Any Alts",
+            "timmyfimple",
+            "Kiyah09",
+            "StinkyObama / HotArjunHot / Any Alts",
+            "Ali14.",
+            "Charlott1626 / CH4R"
+        ]
+    },
+    "Habbo Intelligence Agency": {
+        "link": "https://www.hiahabbo.com/bos--np.html",
+        "usernames": [
+            "SoPhIstiC@TioNS  ",
+            "Freedom996  ",
+            "jurian771/jurian71/juriann71  ",
+            "Poseide  ",
+            "Mannyy  ",
+            "Anonymity/vaporizer  ",
+            "Armatus  ",
+            "Crystal  ",
+            "lil!devil!/TomJ/Zotad  ",
+            "SeaFury  ",
+            "Kemyst  ",
+            "Plalism  ",
+            "-,Shadow,-/Disassociate  ",
+            "Valentino  ",
+            "Jollity  ",
+            "Sohelpmealex  ",
+            "lmaojkitty/Verbal  ",
+            "Aelonis  ",
+            "AliasHog/Yaeloman  ",
+            "Crayans  ",
+            "Ganaram/Ganaham/G4n/Gana  ",
+            "Lucifist/Soprano  ",
+            "Macaw  ",
+            "Menacers  ",
+            "Metromental/Effectivity/Finchy/YFL  ",
+            "Rethrone/motolee28  ",
+            "Skotesh  ",
+            "SmurfPapas/Erebes  ",
+            "Ali21/Ali14  ",
+            "Genovese  ",
+            "God  ",
+            "Jedicon/Egotist/QuarantineHabbo/FruitJellies  ",
+            "barbersyeds  ",
+            "supbruvfam  ",
+            "Mooody  ",
+            "jackson8ryan/Tumon360  ",
+            "Onomanopia/understudy  ",
+            "Valore  ",
+            "Will_vt  ",
+            "M-?  ",
+            "iWail/iWail-NL  ",
+            "Aestrol/Ezco/TheRealAestrol/Tripical  ",
+            "Serpented/Drenched  ",
+            "Optimisation  ",
+            "68What/Airborne68W/DUSTOFF/GermanyBoi  ",
+            "Cupcakes.../NotKayla./Hateable  ",
+            "Billionnames/BrunoChrist  ",
+            "Mahigan/Kodaz  ",
+            "Verol/ThePoint  ",
+            "cfc.forever  "
+        ]
+    }
+}
+
+document.getElementById("bos-manual-check").addEventListener("click", async function () {
+    const tableBody = document.getElementById("bos-results");
+
+    try {
+        const usernameInput = document.getElementById("bos-manual-input").value;
+        tableBody.innerHTML = "";
+
+        for (const agency in boslist) {
+            const { link, usernames } = boslist[agency];
+
+            for (const entry of usernames) {
+                //console.log(`checking "${usernameInput}" against "${entry}"`); // debugging , early stage
+                if (entry.toLowerCase().includes(usernameInput.toLowerCase())) {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                        <td>${agency}</td>
+                        <td><a href="${link}" target="_blank">${agency}</a></td>
+                    `;
+                    tableBody.appendChild(row);
+                    found = true;
+                }
+            }
+        }
+    } catch (error) {
+        console.error("Error during BOS check:", error);
+    }
+});
